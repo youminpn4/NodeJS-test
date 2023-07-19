7ccb234903f8b95128d8ca077f9ed40753c9bfa0
MinCHk <96041119+youminpn4@users.noreply.github.com>
2023-07-20 00:39:28 +0700

    add html template
    ยังไม่ได้แก้หน้าเว็บ

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/" , (req,res) =>{
 
