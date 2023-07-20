3bfa27eb7b74415370abc1fc14d23e1eeb3058ab
MinCHk <96041119+youminpn4@users.noreply.github.com>
2023-07-20 07:58:30 +0700

    app

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
